a4da421e0ad55986aa3ca25c0000f5b6350a01cf
justanotherrandomcoder3 <125522863+justanotherrandomcoder3@users.noreply.github.com>
2023-10-10 19:10:36 -0500

    Create GPT.py
    The code for the bot.

---
+++ GPT.py
@@ -0,0 +1,20 @@
+import socket
+
+class chatquery(striquery):  
+    while True:
+        striquery = input("What do you need assistance with?")
+ client = socket.socket(www.google.com)
+print(socket.listen(client + "/" + striquery));
+
+class SafeSearch:
+    while True:     
+         if chatquery: SafeSearch[chatquery(SafeSearch)];
+opquery = input("What do you need assistance with?")
+client = socket.socket(www.google.com)
+socket.socket(client)
+safemode = ("/safe=active")
+print(socket.listen(client + "/" + opquery + safemode))
+
+
+while True:
+        chatquery: SafeSearch[chatquery(SafeSearch)]
